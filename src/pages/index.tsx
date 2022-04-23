acfe3a0a31de60191ad8352415a7f3b10479b204
ElcioCestari <elciotaira@gmail.com>
2022-04-23 20:33:52 -0300

    titulo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600">
-    <span className="text-4xl">Text</span>
-    </div>
-  )
+      <div className={`flex justify-center items-center
+        h-screen bg-gradient-to-r from-blue-500 to-purple-500
+        text-white
+      `}>
+        <Layout titulo='Cadastro Simples'>
+          <span>Conteudo</span>
+        </Layout>
+      </div>
+
+    )
 }
