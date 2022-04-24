6e84f26a9de035ba8535ee7a1f6a582010dfb141
ElcioCestari <elciotaira@gmail.com>
2022-04-24 16:21:30 -0300

    finish table

--- src/components/tabela.tsx
+++ src/components/tabela.tsx
@@ -1,37 +1,69 @@
 import Cliente from "../core/cliente";
+import { Delete, Edit } from "./icons";
 
 interface TabelaProps {
   clientes: Cliente[];
+  clienteSelecionado?: (cliente: Cliente) => void;
+  clienteExcluido?: (cliente: Cliente) => void;
 }
-
 function Tabela(props: TabelaProps) {
 
+  const exibirAcoes = props.clienteExcluido || props.clienteSelecionado;
   function cabecalho() {
     return (
       <tr>
-        <th>Codigo</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Codigo</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
+        {exibirAcoes ? <th className="justify-center p-4">Ações</th> : false}
       </tr>
     );
   }
 
-  const dados = () => props.clientes?.map((v: Cliente, i)  => {
-    const color =  i % 2 === 0 ? `bg-slate-200` : `bg-slate-300`
+  const renderAcoes = (cliente: Cliente) => {
     return (
-        <tr className={color} key={v.id}>
-            <td>{v.id}</td>
-            <td>{v.nome}</td>
-            <td>{v.idade}</td>
-        </tr>
+      <td className="p-4 flex justify-center">
+        {props.clienteSelecionado ? (
+          <button onClick={() => props.clienteSelecionado?.(cliente) } className="flex justify-center text-red-500 rounded-full hover:bg-purple-50 p-1">
+            {Delete}
+          </button>
+        ) : (
+          false
+        )}
+        {props.clienteExcluido ? (
+          <button onClick={() => props.clienteExcluido?.(cliente)} className="flex justify-center text-green-600 rounded-full hover:bg-purple-50 p-1">
+            {Edit}
+          </button>
+        ) : (
+          false
+        )}
+      </td>
     );
-  });
+  };
+
+  const dados = () =>
+    props.clientes?.map((cliente: Cliente, i) => {
+      const color = i % 2 === 0 ? `bg-purple-200` : `bg-purple-300`;
+      return (
+        <tr className={color} key={cliente.id}>
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+          { exibirAcoes ? renderAcoes(cliente) : false}
+        </tr>
+      );
+    });
 
   return (
-    <table>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`bg-gradient-to-r from-purple-500 to-purple-800 text-gray-100`}
+      >
         {cabecalho()}
-        {dados()}
-    </table>);
+      </thead>
+      <tbody>{dados()}</tbody>
+    </table>
+  );
 }
 
 export default Tabela;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,29 @@
-import Layout from '../components/layout'
-import Tabela from '../components/tabela'
-import Cliente from '../core/cliente'
+import Layout from "../components/layout";
+import Tabela from "../components/tabela";
+import Cliente from "../core/cliente";
 
 export default function Home() {
+  const clientes = [new Cliente("Ana", 31, "1"), new Cliente("Pedro", 30, "2")];
 
-  const clientes = [
-    new Cliente('Ana', 31, '1'),
-    new Cliente('Pedro', 30, '2'),
-  ]
+  const clienteSelecionado = (cliente: Cliente) => {
+    console.log(cliente.nome);
+  };
+  const clienteExcluido = (cliente: Cliente) => {};
 
   return (
-      <div className={`flex justify-center items-center
+    <div
+      className={`flex justify-center items-center
         h-screen bg-gradient-to-r from-blue-500 to-purple-500
         text-white
-      `}>
-        <Layout titulo='Cadastro Simples'>
-        <Tabela clientes={clientes}></Tabela>
-        </Layout>
-      </div>
-
-    )
+      `}
+    >
+      <Layout titulo="Cadastro Simples">
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
+    </div>
+  );
 }
